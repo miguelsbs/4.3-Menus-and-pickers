b152d8a10c6591adae51f22523363451bafce0cd
Miki <miguelsanblassanchez@gmail.com>
2019-03-02 19:24:14 +0100

    4.2:Input_controls_Task 1: Experimentar con atributos de entrada de texto

--- app/src/main/java/com/example/DroidCafeInput/MainActivity.java
+++ app/src/main/java/com/example/DroidCafeInput/MainActivity.java
@@ -1,9 +1,8 @@
-package com.example.droidcafe;
+package com.example.DroidCafeInput;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
--- app/src/main/java/com/example/DroidCafeInput/OrderActivity.java
+++ app/src/main/java/com/example/DroidCafeInput/OrderActivity.java
@@ -1,4 +1,4 @@
-package com.example.droidcafe;
+package com.example.DroidCafeInput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
